7c7c523ef527f42c3353bd03d99255ad516bf02c
chickendoritos <nolakpd@gmail.com>
2018-07-31 23:34:52 -0500

    added local storage persistance

--- src/store.js
+++ src/store.js
@@ -9,12 +9,53 @@ import rootReducer from './reducers/index';
 import sections from './data/sections';
 import items from './data/items';
 
-const defaultState = {
-    sections,
-    items
+// const defaultState = {
+//     sections,
+//     items
+// };
+
+
+
+const loadState = () => {
+    //console.log("start");
+    try {
+        const state = localStorage.getItem('state');
+        //console.log("state", state);
+        if (state === null) {
+            //console.log("null",state);
+
+            return {
+                //state:
+                //{
+                    sections,
+                    items
+                //}
+            };
+        }
+        return JSON.parse(state);
+    } catch (error) {
+        //console.log("err");
+    }
+
+    //console.log("fin");
 };
 
-const store = createStore(rootReducer, defaultState);
+const saveState = (state) => {
+    try {
+        const serState = JSON.stringify(state);
+        localStorage.setItem('state', serState);
+    } catch (error) {
+
+    }
+};
+
+const persistState = loadState();
+//console.log("persistState", persistState);
+const store = createStore(rootReducer, persistState);
+store.subscribe(() => {
+    //console.log("subscribe",store.getState());
+    saveState(store.getState());
+});
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
